6ea4428353e2a70120ff684053f31554386dbe87
JHercules <JHercules_qz@qq.com>
2021-10-11 15:13:26 +0800

    [Hulk]javac

---
+++ langtools/src/share/classes/com/sun/tools/javactest/JavacTest.java
@@ -0,0 +1,10 @@
+package com.sun.tools.javactest;
+
+/**
+ * @author JHercules
+ */
+public class JavacTest {
+    public static void main(String[] args) {
+        System.out.println("hello javac...");
+    }
+}
